ceddf15ffbdd057bc8e8e550b94369ee8fbe4ac7
jarekratajski <jratajski@gmail.com>
2014-11-02 13:42:47 +0100

    show offer

--- app/js/restaurant.js
+++ app/js/restaurant.js
@@ -1,133 +1,73 @@
 /* 
  * global:angular
  */
-//this function will be called after page is initialized
-var restaurant = angular.module("restaurant",[]);
-
-/*
-jQuery(function () {
-    jQuery("section").on("click", "figure", function () {
-        var selected = jQuery(this).hasClass("selected");
-        jQuery("section figure.selected").removeClass("selected");
-        if (!selected) {
-            jQuery(this).addClass("selected");
-        }
-    });
-    var tableCtrl = tableController(tableService());
-    tableCtrl.redraw();
-});*/
+var restaurant = angular.module("restaurant", []);
 
 
-restaurant.factory('tableService', [ function  () {
-    var tables = [
-        {
-            id: 1,
-            state: "FREE"
-        },
-        {
-            id: 2,
-            state: "OCCUPIED"
-        },
-        {
-            id: 3,
-            state: "OCCUPIED"
-        },
-        {
-            id: 4,
-            state: "FREE"
-        },
-        {
-            id: 5,
-            state: "FREE"
-        },
-        {
-            id: 6,
-            state: "OCCUPIED"
-        }
-    ], findTable = function (id) {
-        for (var index in tables) {
-            if (tables[index].id === id) {
-                return tables[index];
+
+restaurant.factory('tableService', ['$http',function ($http) {
+        var tablesData = { tables: [], offer: [] } 
+        , findTable = function (id) {
+            for (var index in tablesData.tables) {
+                if (tablesData.tables[index].id === id) {
+                    return tablesData.tables[index];
+                }
             }
-        }
-    };
+        };
 
 
-    return {
-        getTables: function () {
-            return tables;
-        },
-        occupy: function (id) {
-            var table = findTable(id);
-            table.state = "OCCUPIED";
-            this.markTable(table);
-        }, 
-        free: function (id) {
-            var table = findTable(id);
-            table.state = "FREE";
-            this.markTable(table);
-        }, findTable : findTable,
-        getFromServer : function( callback ) {
-            jQuery.get('/services/rest/tablemanagement/table/', function(data) {
-                tables = data;
-                callback(tables);
-            });
-        },
-        markTable : function (table, callback) {
-            jQuery.post( '/services/rest/tablemanagement/table/'+table.id+'/markTableAs'+table.state);
-            
-        }
-    };
-}]);
-
-restaurant.controller('tableController', [ '$scope', 'tableService', function ($scope, tableService) {
-    var addTableButtons = function (tableElem, id) {
-        var menu = jQuery("<menu>");
-        var occupyButton = jQuery("<button>OCCUPY</button>");
-        occupyButton.on("click", function () {
-            tableService.occupy(id);
-            updateTable(tableElem,id);
-        });
-        var freeButton = jQuery("<button>FREE</button>");
-        freeButton.on("click", function () {
-            tableService.free(id);
-            updateTable(tableElem,id);
-        });
-        menu.append(occupyButton);
-        menu.append(freeButton);
-        tableElem.append(menu);
-    }, updateTable = function(tableElem, id) {
-        var table = tableService.findTable(id);
-        tableElem.attr('data-state', table.state);
-    };
-    
-    $scope.tables = tableService.getTables();
-
-  /*  return {
-        makeTable: function (table) {
-            var figTable = jQuery("<figure>");
-
-            var figTableCaption = jQuery("<figcaption>").text(table.id);
-            figTable = figTable.append(figTableCaption);
-            figTable.attr('data-state', table.state);
-            figTable.addClass("table");
-            figTable.attr("data-id", table.id);
-            addTableButtons(figTable, table.id);
-            return figTable;
-        },
-        drawTables: function () {
-            var root = jQuery("section"),
-                    allTables = tableService.getTables();
-
-            for (var i = 0; i < allTables.length; i++) {
-                root.append(this.makeTable(allTables[i]));
+        return {
+            getTables: function () {
+                this.getFromServer();
+                return tablesData;
+            },
+            occupy: function (id) {
+                var table = findTable(id);
+                table.state = "OCCUPIED";
+                this.markTable(table);
+            },
+            free: function (id) {
+                var table = findTable(id);
+                table.state = "FREE";
+                this.markTable(table);
+            }, findTable: findTable,
+            getFromServer: function () {
+                if (tablesData.tables.length <= 0)  {
+                    $http.get('/services/rest/tablemanagement/table/')
+                            .success(function(data) {
+                                tablesData.tables = data;
+                            });
+                    $http.get('/services/rest/offermanagement/offer')
+                            .success(function(data) {
+                                tablesData.offer = data;
+                            });
+                }
+                
+            },
+            markTable: function (table) {
+                $http.post('/services/rest/tablemanagement/table/' + table.id + '/markTableAs' + table.state);
             }
-        },
-        redraw: function () {
-            var self = this;
-            tableService.getFromServer( function() { self.drawTables(); } );
-        }
-        
-    };*/
+        };
+    }]);
 
-}]);;
+restaurant.controller('tableController', ['$scope', 'tableService', function ($scope, tableService) {
+        $scope.selected = null;
+        $scope.tablesData = tableService.getTables();
+        $scope.isSelected = function (table) {
+            return table.id === $scope.selected ? "selected" : "";
+        };
+        $scope.select = function (table) {
+            $scope.selected = table.id;
+        };
+        $scope.occupy = function (table) {
+            tableService.occupy(table.id);
+        };
+        $scope.free = function (table) {
+            tableService.free(table.id);
+        };
+        $scope.close = function (table) {
+            $scope.selected = null;
+            return false;
+        };
+    }]);
+;
